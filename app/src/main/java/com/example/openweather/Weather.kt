e06ea6a5c0195657f49807b6b2520da8bd3d08cc
Orlandroid <lopez.orlando.gold9@gmail.com>
2021-10-08 20:15:39 -0500

    Add screen maps and wheather and ViewBinding

---
+++ app/src/main/java/com/example/openweather/Weather.kt
@@ -0,0 +1,29 @@
+package com.example.openweather
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.openweather.databinding.FragmentWeatherBinding
+
+
+class Weather : Fragment() {
+
+    private var _binding: FragmentWeatherBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWeatherBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
